a92dd85a9715d8f5d5b9a83f9971b32d59e3a8c3
Olbor <32017465+OlborEgamorf@users.noreply.github.com>
2024-05-15 20:37:23 +0200

    Evaluation tags et filtre

--- musicCaps/evaluateData.py
+++ musicCaps/evaluateData.py
@@ -5,14 +5,31 @@ def readCSV(filename:str) -> list[dict]:
 
     table = []
     file = open(filename+".csv", encoding="utf-8-sig", newline="\n")
-    for ligne in csv.DictReader(file,delimiter=";"):
+    for ligne in csv.DictReader(file,delimiter=","):
         table.append(dict(ligne))
     file.close()
 
     return table
 
+def toCSV(final,fileName):
 
-table = readCSV("musiccaps-public")
+    file = file=open(f"{fileName}.csv", "w", encoding="utf-8-sig")
+
+    ajout_key=""
+    for key in final[0].keys():
+        ajout_key=f"{key};"
+
+    file.write(ajout_key[:-1])
+
+    for i in range(len(final)):
+        ajout_value=""
+        for value in final[i].values():
+            ajout_value+=f"{value};"
+
+        file.write("\n"+ajout_value[:-1])
+
+
+table = readCSV("musiccaps-public-filter")
 data = {}
 somme = 0
 
@@ -21,7 +38,8 @@ for i in table:
     tags = tags.replace("[","")
     tags = tags.replace("]","")
     tags = tags.replace("'","")
-    t = tags.split("; ")
+    t = tags.split(", ")
+    i["tags"] = t
     for j in t:
         if j not in data:
             data[j] = 0
@@ -30,3 +48,26 @@ for i in table:
 
 print(dict(sorted(data.items(), key=lambda item: item[1], reverse=True)))
 print(len(data), somme/len(data))
+print(len(table))
+
+
+p = 0
+for i in table:
+    for z in i["tags"]:
+        if data[z] >= 20:
+            p += 1
+            break
+
+print(p)
+
+l = []
+m = 0
+d = []
+for i in data:
+    if data[i] >= 20:
+        d.append(i)
+        l.append({"id":m,"nom":i,"count":data[i]})
+        m += 1
+    
+
+toCSV(l,"tags20")
