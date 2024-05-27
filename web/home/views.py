0e7805fe145c9eed110256d8f027035899b9ed5f
drikomak <67830361+drikomak@users.noreply.github.com>
2024-05-27 14:52:50 +0200

    demonstration

--- web/home/views.py
+++ web/home/views.py
@@ -1,6 +1,10 @@
 import time
 from django.shortcuts import render
 from home.func import getEmotion, initModel, gen, randomID
+import os
+from django.http import HttpResponse
+from django.conf import settings
+import csv
 
 ids = []
 model = None
@@ -8,8 +12,37 @@ init = True
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, "home/index.html", {})
+    audio_data = []
+
+    try:
+        with open('record.csv', newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=';')
+            for row in reader:
+                audio_data.append({
+                    'name': row['path'],  # Assuming 'path' contains the file name
+                    'prompt': row['prompt'],
+                    'url': os.path.join(settings.MEDIA_URL, row['path'])
+                })
+    except FileNotFoundError:
+        return HttpResponse("Le fichier CSV est introuvable.")
+    except Exception as e:
+        return HttpResponse(f"Une erreur s'est produite: {str(e)}")
+    
+    media_path = settings.MEDIA_ROOT
+    available_audio_files = []
+    if os.path.exists(media_path):
+        for file_name in os.listdir(media_path):
+            if file_name.endswith('.wav'):
+                for audio in audio_data:
+                    if audio['name'] == file_name:
+                        available_audio_files.append(audio)
+                        break
+    else:
+        return HttpResponse("Le chemin des médias n'existe pas.")
+
+    return render(request, 'home/index.html', {'audio_files': available_audio_files})
 
 def api(request):
     
@@ -34,5 +67,25 @@ def api(request):
 def camera(request):
     return render(request, "home/face.html", {"emotion":getEmotion()})
 
+
+
+def get_audio_files(request):
+    media_path = settings.MEDIA_ROOT
+    print(f"Checking media path: {media_path}")  # Debug
+    if not os.path.exists(media_path):
+        print("Media path does not exist")  # debug
+        return render(request, 'error.html', {'error': 'Media path does not exist'}, status=400)
+
+    audio_files = []
+    for file_name in os.listdir(media_path):
+        if file_name.endswith('.wav'):
+            print(f"Found audio file: {file_name}")  # debug
+            audio_files.append({
+                'name': file_name,
+                'url': os.path.join(settings.MEDIA_URL, file_name)
+            })
+    
+    print(f"Returning audio files: {audio_files}")  # debug
+    return render(request, 'audio_files.html', {'audio_files': audio_files})
 
 ids, model = initModel(init)
